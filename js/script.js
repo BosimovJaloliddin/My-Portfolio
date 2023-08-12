27274357ba0c0ec9e1d87f977700cf5b75052735
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-08-13 00:50:42 +0500

    add style switcher

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ===================== Typeing animation====================== //
+var typed = new Typed(".typing", {
+  strings: ["Web Developer", "Frontend Developer"],
+  typeSpeed: 100,
+  BackSpeed: 65,
+  loop: true,
+});
---
+++ js/style-switcher.js
@@ -0,0 +1,41 @@
+// ==================== switcher toggle ================== //
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+const styleSwitcher = document.querySelector(".style-switcher");
+
+styleSwitcherToggle.addEventListener("click", () => {
+  styleSwitcher.classList.toggle("open");
+});
+
+// hide style -switcher on scroll
+window.addEventListener("scroll", () => {
+  if (styleSwitcher.classList.contains("open")) {
+    styleSwitcher.classList.toggle("open");
+  }
+});
+
+// ==================== theme color ================== //
+
+const altenateStyle = document.querySelectorAll(".alternate-style");
+
+const setActiveStyle = (color) => {
+  altenateStyle.forEach((value) => {
+    color === value.getAttribute("title")
+      ? value.removeAttribute("disabled")
+      : value.setAttribute("disabled", true);
+  });
+};
+
+// ==================== theme light and dark mode ================== //
+
+const dayNight = document.querySelector(".day-night");
+
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+});
+// window.addEventListener("load", () => {
+//   document.body.classList.contains("dark")
+//     ? dayNight.querySelector("i").classList.add("fa-sun")
+//     : dayNight.querySelector("i").classList.add("fa-moon");
+// });
