47e0adb53ed518b6217247ca194ff9c2a72306b2
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-08-14 18:25:25 +0500

    add js code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
-// ===================== Typeing animation====================== //
+// ===================== Typeing animation ====================== //
 var typed = new Typed(".typing", {
   strings: ["Web Developer", "Frontend Developer"],
-  typeSpeed: 100,
+  typeSpeed: 300,
   BackSpeed: 65,
   loop: true,
+});
+
+// ===================== Aside ====================== //
+const nav = document.querySelector(".nav");
+const navList = document.querySelectorAll("li");
+const totalNavList = navList.length;
+const sections = document.querySelectorAll("section");
+
+const showSection = (element) => {
+  let target = element.getAttribute("href").slice(1);
+
+  sections.forEach((v) => {
+    v.getAttribute("id") === target
+      ? v.classList.add("active")
+      : v.classList.remove("active");
+  });
+  document.querySelector("#" + target).classList.add("active");
+};
+
+navList.forEach((v) => {
+  const a = v.querySelector("a");
+
+  a.addEventListener("click", () => {
+    navList.forEach((v) => {
+      v.querySelector("a").classList.remove("active");
+    });
+    a.classList.add("active");
+    showSection(a);
+  });
 });
